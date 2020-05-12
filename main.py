17e71650fd55df1b1b64199afefe98d8d99da25d
larrygf <larrywtf609@gmail.com>
2020-05-12 19:06:21 +0100

    Working version

--- main.py
+++ main.py
@@ -57,6 +57,66 @@ def process_xml_json(xml_json: dict,csv_json:dict):
             else:
                 raise Exception('Script not prepaired for this structure')
     return xml_json
+
+def adapt_json_to_csv(list_to_adapt:dict):
+    big_list = []
+    for measurement in list_to_adapt:
+        if type(measurement['MO']) == list:
+            MO_keys = list(measurement['MO'][0].keys())
+            MO_keys = []
+            temp_list = []
+            for index,value in enumerate(measurement['MO']):
+                temp_list.append(list(value.values()))
+        
+            len_temp_list = len(temp_list)
+            len_inner_lists = len(temp_list[0])
+            count1 = 0
+            count2 = 0
+            final_list = []
+            while count1 < len_inner_lists:
+                small_list = []
+                while count2 < len_temp_list:
+                    small_list.append(temp_list[count2][count1])
+                    count2+=1
+                final_list.append(small_list)
+                count1+=1
+                count2=0
+            
+            MO_values = [tuple(element) for element in final_list]
+           
+            
+           
+        else:
+            MO_keys = list(measurement['MO'].keys())
+            MO_values = list(measurement['MO'].values())
+        
+        new_MO_keys = []
+        for key in MO_keys:
+            if '@' in key:
+                new_MO_keys.append(key.replace('@',''))
+            else:
+                new_MO_keys.append(key)
+        
+        MO_keys = new_MO_keys
+        
+        Net_keys = list(measurement['NE-WBTS_1.0'].keys())
+        
+        new_Net_keys = []
+        for key in Net_keys:
+            if '@' in key:
+                new_Net_keys.append(key.replace('@',''))
+            else:
+                new_Net_keys.append(key)
+        Net_keys = new_Net_keys
+        Net_values = list(measurement['NE-WBTS_1.0'].values())
+        column_list =  MO_keys + Net_keys 
+        values_list =  MO_values + Net_values
+        big_list.append(column_list)
+        big_list.append(values_list)
+        big_list.append('')
+    
+    return big_list
+        
 if __name__ == "__main__":
     csv_path = click.prompt('Path or name of the csv file ',default='2.csv')
     csv_json = csv_to_json(csv_path)
@@ -71,6 +131,13 @@ if __name__ == "__main__":
     with open('processed_xml.json','w') as file:
         json.dump(processed_xml_json,file, indent=2)
         
+    count10 = 0
     for date in processed_xml_json:
-        dict_to_adapt = processed_xml_json[date]
+        list_to_adapt = processed_xml_json[date]
+        list_of_lists = adapt_json_to_csv(list_to_adapt)
+        new_date = date.replace('-','_').replace(':','-')
         
+        with open(new_date+'.csv','w', newline='') as file:
+            writer =  csv.writer(file)
+            writer.writerows(list_of_lists)
+        count10+=1
